a9abffb3a87269b6c85d9cb262803c6388a5e8ce
Austin Scrivener <ostim12121212@gmail.com>
2025-03-10 10:58:12 +1300

    2

--- obj/Debug/net8.0/benchmark.AssemblyInfo.cs
+++ obj/Debug/net8.0/benchmark.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("benchmark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65459ab243160529516d61388a95275aee089299")]
 [assembly: System.Reflection.AssemblyProductAttribute("benchmark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("benchmark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
